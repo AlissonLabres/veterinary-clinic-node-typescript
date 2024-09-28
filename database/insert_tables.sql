d7c0ccae6093029d30b6d34943383753f01af577
Alisson Labres Gimenes <alisson.labres@gmail.com>
2024-09-28 01:00:37 -0300

    [CHORE] - Create file jest config and add stryker to mutant test

--- database/insert_tables.sql
+++ database/insert_tables.sql
@@ -1,11 +1,14 @@
+INSERT INTO users(user_name, user_email, user_phone) 
+VALUES ('John Due', 'email@email.com', '99999999999');
+
 INSERT INTO medical(medical_name, medical_specialities, medical_phone, medical_email) 
 VALUES ('Evelyn Melo Ferreira', 'GENERAL_DOCTOR,VACCINATION', '2140696358', 'evelynmeloferreira@veterinaryclinical.com.br');
 
-INSERT INTO medical(medical_name, medical_specialities, medical_phone, medical_email) 
-VALUES ('Enzo Rodrigues Cavalcanti', 'GENERAL_DOCTOR,VACCINATION', '2130339267', 'enzorodriguescavalcanti@veterinaryclinical.com.br');
+INSERT INTO animal(animal_name, animal_breed, animal_age, animal_weight, animal_type, user_id)
+VALUES ('Scott', 'N/A', 5, 5, 'CAT', 1);
 
-INSERT INTO bullet(bullet_code) VALUES ('2023-10-08T16:00');
-INSERT INTO bullet(bullet_code) VALUES ('2023-10-30T16:00');
-INSERT INTO bullet(bullet_code) VALUES ('2023-10-27T16:00');
-INSERT INTO bullet(bullet_code) VALUES ('2023-10-27T16:00');
-INSERT INTO bullet(bullet_code) VALUES ('2023-10-27T16:00');
+INSERT INTO bullet(bullet_code) VALUES ('2024-09-08T16:00');
+INSERT INTO bullet(bullet_code) VALUES ('2024-09-30T16:00');
+INSERT INTO bullet(bullet_code) VALUES ('2024-09-27T16:00');
+INSERT INTO bullet(bullet_code) VALUES ('2024-09-27T16:00');
+INSERT INTO bullet(bullet_code) VALUES ('2024-09-27T16:00');
